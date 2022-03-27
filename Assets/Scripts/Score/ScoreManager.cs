6026eb7522e5761f5760b9ed28fcfd671554ef0f
Takayama <823ab172@gmail.com>
2022-03-28 03:06:21 +0900

    追加:スコアアイテムクラスとスコアテキスト表示を追加

---
+++ Assets/Scripts/Score/ScoreManager.cs
@@ -0,0 +1,25 @@
+using UniRx;
+using UnityEngine;
+
+namespace Score.Manager
+{
+    public class ScoreManager : MonoBehaviour
+
+    {
+    public IReadOnlyReactiveProperty<int> Score => score;
+    private readonly IntReactiveProperty score = new IntReactiveProperty(0);
+
+    /// <summary>
+    /// 渡された引数スコアを加算するメソッド 
+    /// </summary>
+    public void IncreaseScore(int scoreOffset)
+    {
+        score.Value += scoreOffset;
+    }
+
+    private void OnDestroy()
+    {
+        score.Dispose();
+    }
+    }
+}
---
+++ Assets/Scripts/Score/ScorePresenter.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using Score.Manager;
+using UniRx;
+using UI;
+
+namespace Score.Presenter
+{
+    /// <summary>
+    /// スコアテキストとスコアマネージャーのプレゼンタークラス
+    /// </summary>
+    public sealed class ScorePresenter : MonoBehaviour
+    {
+        [SerializeField] private ScoreManager scoreManager;
+        [SerializeField] private ScoreText scoreText;
+
+        private void Start()
+        {
+            scoreManager.Score
+                .Subscribe(score =>
+                    scoreText.SetText(score))
+                .AddTo(this);
+        }
+    }   
+}
