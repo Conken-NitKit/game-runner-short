efaeed208b3563a4b4bb720975c654382b7817f5
平田海輝 <seaglory2004@gmail.com>
2022-03-30 07:16:36 +0900

    改名: タイトルパネルのスクリプトをTitleImageScript→TitlePanelに改名しました。

--- Assets/Scripts/Title/TitlePanel.cs
+++ Assets/Scripts/Title/TitlePanel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TitleImageScript : MonoBehaviour
+public class TitlePanel : MonoBehaviour
 {
     [SerializeField] Sprite[] sprites;
     [SerializeField] AudioClip se;
