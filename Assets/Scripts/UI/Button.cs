254bbd83d74b57013e134603256ad5e027ee3f0d
平田海輝 <seaglory2004@gmail.com>
2022-03-30 08:43:26 +0900

    更新: ボタンのクラスに親を格納する変数を追加しました。

--- Assets/Scripts/UI/Button.cs
+++ Assets/Scripts/UI/Button.cs
@@ -2,4 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button : MonoBehaviour { }
+public class Button : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+}
