8dd0279525b0e44fcb7abc2b9da2f8dc57e2aed5
平田海輝 <seaglory2004@gmail.com>
2022-03-30 11:23:41 +0900

    削除: タイトルパネルからAudio Sourceを削除しました。

--- Assets/Scripts/Anime.cs
+++ Assets/Scripts/Anime.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TitlePanel : MonoBehaviour
+public class Anime : MonoBehaviour
 {
     [SerializeField] Sprite[] sprites;
     Image image;
