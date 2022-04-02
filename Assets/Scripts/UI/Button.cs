1af0e89e9e637020862e5fcb5d24d5c188ead197
平田海輝 <seaglory2004@gmail.com>
2022-04-02 19:08:06 +0900

    更新: ボタンのクラスにsummaryを追加しました。

--- Assets/Scripts/UI/Button.cs
+++ Assets/Scripts/UI/Button.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 主にボタンのアニメーション
+/// </summary>
 public class Button : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
