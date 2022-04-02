2d815225cac2c988e0d6e5981b091f5cbcf96f09
平田海輝 <seaglory2004@gmail.com>
2022-04-02 19:03:49 +0900

    更新: 色クラスにsummaryを追加しました。

--- Assets/Constants/COLORS.cs
+++ Assets/Constants/COLORS.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 色
+/// </summary>
 public class COLORS : MonoBehaviour
 {
     public Color BUTTON = Color.yellow;
